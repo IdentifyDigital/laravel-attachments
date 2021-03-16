8b2ef0caa52d4ff3dee54a24476a14153af4ef75
Bradley King <contact@bradley-king.com>
2021-03-16 08:41:41 +0000

    Copy from GitHub
    To Be Improved on the new private suite

---
+++ src/AttachmentsServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace IdentifyDigital\Attachments;
+
+use Illuminate\Support\ServiceProvider;
+use IdentifyDigital\Contacts\Models\Address;
+use IdentifyDigital\Contacts\Observers\AddressObserver;
+
+/**
+ * Class AttachmentsServiceProvider
+ * @package IdentifyDigital\Attachments
+ */
+class AttachmentsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+    }
+
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
